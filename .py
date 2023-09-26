c8235a915fc2dc24938faf4e30fc614afbb8eb75
jussara2 <freitasjussara887@gmail.com>
2023-09-26 14:30:09 -0300

    avaliacao do 1d

---
+++ .py
@@ -0,0 +1,7 @@
+num1=(input("digite seu nome"))
+num2 = int(input("digite o ano atual"))
+num3 = int(input("digite o ano em que voce nasceu"))
+total = num2 - num3
+print("ola",num1,"sua idade é",total)
+
+
